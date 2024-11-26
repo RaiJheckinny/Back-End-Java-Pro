7e821f3193e53fa7e803961033a493a1573da351
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-11-26 18:06:08 -0300

    feat: exercio de Fila

--- modulo1/src/Main.java
+++ modulo1/src/Main.java
@@ -13,10 +13,21 @@ public class Main {
         pilha.push(5);
 
         //Pilha o ultimo a entrar e o primeiro a sair
+        System.out.println("Pilha");
         System.out.println(pilha.pop());
         System.out.println(pilha.pop());
 
+        //Fifo o primeiro a entrar e o primeiro a sair
+        Queue<Integer> fila = new LinkedList<>();
+        fila.offer(2);
+        fila.offer(6);
+        fila.offer(9);
+        fila.offer(1);
 
-
+        //Fifo o primeiro a entrar e o primeiro a sair
+        System.out.println("Fila");
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
+        System.out.println(fila.poll());
     }
 }

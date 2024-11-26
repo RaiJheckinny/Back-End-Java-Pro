9335eabc38dabc44a18b15ca92843440932a3586
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-11-26 17:58:47 -0300

    feart: exercio de Pilha

--- modulo1/src/Main.java
+++ modulo1/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Stack
+        Stack<Integer> pilha = new Stack<>();
+        pilha.push(1);
+        pilha.push(2);
+        pilha.push(3);
+        pilha.push(4);
+        pilha.push(5);
+
+        //Pilha o ultimo a entrar e o primeiro a sair
+        System.out.println(pilha.pop());
+        System.out.println(pilha.pop());
+
+
+
     }
 }
